6b031283dd2167ead0f254b3f0525a47f6c195e2
Николай Чечулин <chechulin646@live.ru>
2019-07-24 13:06:10 +0300

    generate basic pdf with sections

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import requests
 from pylatex import Document, Section, Subsection, Command
 
 soup = None
+doc = None
 
-
-def add_section(doc, section_name):
+def add_section(section_name):
+    global doc
     with doc.create(Section(section_name)):
-        doc.append('this is section {0}'.format(section_name))
+        doc.append(f'this is section {section_name}')
 
 
 # def fill_document(doc):
@@ -27,15 +28,26 @@ def get_sections():
     global soup
     return soup.findAll('div', {'class': 'section'})
 
+def get_section_title(section_div):
+    a_tag = section_div.find('a', {'class' : 'toc-backref'})
+    return a_tag.text
 
 if __name__ == '__main__':
+    doc = Document('basic')
+
+
     init_soup('https://www.python.org/dev/peps/pep-0008/')
     sections = get_sections()
 
     print(len(sections))
 
+    for sec in sections:
+        add_section(get_section_title(sec))
+
+    doc.generate_pdf(clean_tex=True)
+    doc.generate_tex()
+
     # doc = Document('basic')
     # add_section(doc, 'first one')
 
-    # doc.generate_pdf(clean_tex=True)
-    # doc.generate_tex()
+    
